b355cfd8568b116bc39e6305e9fb4ee53e624d44
santos-project <info@santos.main.jp>
2022-08-25 00:51:18 +0900

    Improve file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path');
-const MiniCssExtractPligin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPligin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/javascripts/main.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, "./dist"),
+    filename: "javascripts/main.js",
   },
   module: {
     rules: [
@@ -16,16 +18,19 @@ module.exports = {
             loader: MiniCssExtractPligin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
         ],
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPligin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
+    new MiniCssExtractPligin({
+      filename: "./stylesheets/main.css",
     }),
-  ]
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
