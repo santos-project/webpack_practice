dbf21ab6e25c3192627bee207837013f1e47d50c
santos-project <info@santos.main.jp>
2022-08-24 23:40:54 +0900

    webpack.config.js css-loader style-loader add

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, './dist')
-  }
-}
+    path: path.resolve(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
