67cbda215c6d4758ee10a6a16cdbb734b205b411
santos-project <info@santos.main.jp>
2022-08-26 09:02:46 +0900

    webpack.config.js pug plugin add

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,22 @@ module.exports = {
             loader: "file-loader",
             options: {
               esModule: false,
-              name: 'images/[name].[ext]',
-            }
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              pretty: true,
+            },
           },
         ],
       },
@@ -41,7 +55,16 @@ module.exports = {
       filename: "./stylesheets/main.css",
     }),
     new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
+      template: "./src/templates/index.pug",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: "access.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/members/taro.pug",
+      filename: "members/taro.html",
     }),
     new CleanWebpackPlugin(),
   ],
