f75cb73d52345c9ca5e3e1f67a9a7601dfd4107b
santos-project <info@santos.main.jp>
2022-08-26 23:40:35 +0900

    webpack.config.js sass plugin add

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPligin.loader,
           },
           {
             loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
           },
         ],
       },
