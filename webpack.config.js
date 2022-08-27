dd421e322b7a9637b6c390a1b5b72d6fb1a86f20
santos-project <info@santos.main.jp>
2022-08-28 00:19:04 +0900

    webpack.config.js filename change

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: "./src/javascripts/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "javascripts/main.js",
+    filename: "javascripts/[name]-[hash].js",
   },
   module: {
     rules: [
@@ -55,7 +55,8 @@ module.exports = {
             loader: "file-loader",
             options: {
               esModule: false,
-              name: "images/[name].[ext]",
+              name: "images/[name]-[hash].[ext]",
+              publicPath: "/",
             },
           },
           {
@@ -88,7 +89,7 @@ module.exports = {
   plugins: [
     new VueLoaderPlugin(),
     new MiniCssExtractPligin({
-      filename: "./stylesheets/main.css",
+      filename: "./stylesheets/[name]-[hash].css",
     }),
     new HtmlWebpackPlugin({
       template: "./src/templates/index.pug",
